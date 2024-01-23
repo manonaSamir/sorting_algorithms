974d7375749497072305c34f184f7d2add46c830
manonaSamir <bena.yalla@yahoo.com>
2024-01-23 20:20:06 +0200

    3- quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,22 +1,5 @@
 #include "sort.h"
 
-/**
- * swap_items - Swaps two items in an array.
- * @array: The array to modify.
- * @l: The index of the left item.
- * @r: The index of the right item.
- */
-void swap_items(int *array, size_t l, size_t r)
-{
-	int tmp;
-
-	if (array != NULL)
-	{
-		tmp = array[l];
-		array[l] = array[r];
-		array[r] = tmp;
-	}
-}
 
 /**
  * quick_sort_range_lomuto - Sorts a sub array using the
@@ -41,7 +24,7 @@ void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size)
 		{
 			if (k != i)
 			{
-				swap_items(array, k, i);
+				swap(array, k, i);
 				print_array(array, size);
 			}
 			k++;
@@ -49,14 +32,35 @@ void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size)
 	}
 	if (k != high)
 	{
-		swap_items(array, k, high);
+		swap(array, k, high);
 		print_array(array, size);
 	}
 	if (k - low > 1)
 		quick_sort_range_lomuto(array, low, k - 1, size);
 	if (high - k > 1)
+
 		quick_sort_range_lomuto(array, k + 1, high, size);
 }
+
+
+/**
+ * swap - Swaps two items.
+ * @array: The array to modify.
+ * @l: The index of the left item.
+ * @r: The index of the right item.
+ */
+void swap(int *array, size_t l, size_t r)
+{
+	int tmp;
+
+	if (array != NULL)
+	{
+		tmp = array[l];
+		array[l] = array[r];
+		array[r] = tmp;
+	}
+}
+
 
 /**
  * quick_sort - Sorts an array using the quick sort algorithm
